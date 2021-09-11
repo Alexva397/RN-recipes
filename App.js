d477e2a24e8bf3cdc211700ff616dcb507f7ea29
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-09-11 17:59:12 -0500

    created app using expo, added screens, installed react navigation packages, added fonts and apploading

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
+export default function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  if (!loaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setLoaded(true)} onError={(err) => console.log(err)}/>;
+   }
+ 
+
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
